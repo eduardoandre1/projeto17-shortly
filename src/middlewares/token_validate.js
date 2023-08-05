734f995b57caefa1907b3e4931414872c6177fdd
eduardo andre <eduardo_andre@id.uff.br>
2023-08-05 19:10:46 -0300

    fix: token validade autorization

--- src/middlewares/token_validate.js
+++ src/middlewares/token_validate.js
@@ -1,11 +1,10 @@
 import DB from "../database/postgresSQL.js"
 
 export default async function tokenvalidade(req,res,next){
-        const token = req.headers['Authorization']
-        console.log(token)
+        const {authorization} = req.headers
         const select = `SELECT * FROM "tokens" WHERE "token" = $1`
         try{
-            const finder = await DB.query(select,[token])
+            const finder = await DB.query(select,[authorization])
             if(finder.rowCount === 0){
             return res.sendStatus(401)}
         }
