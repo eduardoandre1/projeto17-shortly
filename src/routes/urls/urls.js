67f0bef6e23032908149fcca10a60baf195c67ac
eduardo andre <eduardo_andre@id.uff.br>
2023-08-05 14:52:24 -0300

    feat: get url

--- src/routes/urls/urls.js
+++ src/routes/urls/urls.js
@@ -3,8 +3,10 @@ import tokenvalidade from "../../middlewares/token_validate.js";
 import shorten from "../../controllers/shorten_maker.js";
 import input_validate from "../../middlewares/input_validate.js";
 import schema_url from "../../schemas/schema_url.js";
+import list_urls from "../../controllers/list_urls.js";
 
 const urls = Router()
 urls.post('/urls/shorten',input_validate(schema_url),tokenvalidade,shorten)
+urls.get("/url/:id",list_urls)
 
 export default urls
