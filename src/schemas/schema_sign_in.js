9f622b98463380dc7c5f1c06c626cf5d15586e2c
eduardo andre <eduardo_andre@id.uff.br>
2023-08-05 23:33:54 -0300

    fix: te schema of email problem

--- src/schemas/schema_sign_in.js
+++ src/schemas/schema_sign_in.js
@@ -1,7 +1,7 @@
 import Joi from "joi";
 
 const  schema_sign_in = Joi.object({
-    email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).required(),
+    email: Joi.string().email().required(),
     password: Joi.string().required(),
 })
 export default schema_sign_in
