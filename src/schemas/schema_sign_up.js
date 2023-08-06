99cf738bb44323596e9c842d3a232801d97732ef
eduardo andre <eduardo_andre@id.uff.br>
2023-08-05 23:14:41 -0300

    fix:schema sing up in email

--- src/schemas/schema_sign_up.js
+++ src/schemas/schema_sign_up.js
@@ -2,7 +2,7 @@ import Joi from "joi";
 
 const schema_sign_up = Joi.object({
     name: Joi.string().required(),
-    email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).required(),
+    email: Joi.string().email().required(),
     password: Joi.string().pattern(new RegExp('[a-zA-Z0-9]{3,30}$')).required(),
     confirmPassword:Joi.ref('password')
 })
