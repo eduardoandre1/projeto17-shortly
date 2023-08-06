9651b3f800865fb8f041f4c7b1dfe41e09bac8ad
eduardo andre <eduardo_andre@id.uff.br>
2023-08-06 00:20:49 -0300

    feat: viens counter done

--- src/controllers/redirect_url.js
+++ src/controllers/redirect_url.js
@@ -4,8 +4,10 @@ export default async function redirect_url(req,res){
     try{
         const url = await DB.query(`SELECT urls.original_url as url FROM urls WHERE urls.shorter_url = $1`,[short_url])
         if(url.rowCount === 0){
-            return res.status(404).send('shorted url invalided')
+            return res.status(404).send('shorted url is invalid')
         }
+        await DB.query(`UPDATE urls SET viens = viens + 1 WHERE shorter_url =$1`,[short_url])
+        console.log(url.rows[0])
         return res.redirect(url.rows[0].url)
     }catch(err){return res.status(500).send(err.message)}
 
