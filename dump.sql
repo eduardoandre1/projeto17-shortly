e8a672646722c0bd79fc097bbd363713ae725400
eduardo andre <eduardo_andre@id.uff.br>
2023-08-06 12:29:04 -0300

    chore: nem dump to test

--- dump.sql
+++ dump.sql
@@ -61,7 +61,7 @@ CREATE TABLE public.tokens (
     user_id integer NOT NULL,
     id integer NOT NULL,
     token text NOT NULL,
-    "createdAt" timestamp without time zone NOT NULL
+    "createdAt" timestamp with time zone NOT NULL
 );
 
 
@@ -95,7 +95,7 @@ CREATE TABLE public.urls (
     user_id character varying(255) NOT NULL,
     original_url text NOT NULL,
     shorter_url text NOT NULL,
-    viens integer DEFAULT 0 NOT NULL
+    viens integer DEFAULT 0
 );
 
 
@@ -145,62 +145,69 @@ ALTER TABLE ONLY public.urls ALTER COLUMN id SET DEFAULT nextval('public.urls_id
 --
 
 INSERT INTO public."Users" VALUES (1, 'ewd@gmail.com', '$2b$10$rJQx5U8jjxOckI2gKDP8xeF18eSNSg50x5H0mQ8xWTKgUdhJ0KoZe', 'edu', NULL);
+INSERT INTO public."Users" VALUES (3, 'beleza@gmail.com', '$2b$10$2eUlbgdx705fOzKtyzMEQeCGfJSqzudJK5wmckOSY4Qd8jHJlRohe', 'eduardo', NULL);
+INSERT INTO public."Users" VALUES (4, 'beleza@gmail.com.br', '$2b$10$EekHNVSNu8UmhlzmCEGCRe/9NBlUdF/fzTBjDB19Tp9IMB.rt8MGC', 'eduardo', NULL);
 
 
 --
 -- Data for Name: tokens; Type: TABLE DATA; Schema: public; Owner: -
 --
 
-INSERT INTO public.tokens VALUES (1, 2, '42fa7213-c2be-4d33-a0f9-2250202d0a0d', '2023-08-03');
-INSERT INTO public.tokens VALUES (1, 3, '1de75c83-1c9a-4ff7-af72-b1d02229a6a8', '2023-08-03');
-INSERT INTO public.tokens VALUES (1, 4, 'f2b8fee2-b977-46d2-9f60-19735ebd50ed', '2023-08-03');
-INSERT INTO public.tokens VALUES (1, 5, 'e2cc1cd2-4c9c-4fca-913e-a5fe76e08bdf', '2023-08-03');
-INSERT INTO public.tokens VALUES (1, 6, 'de3bcf9c-db98-4b59-9692-92c18c2c9f23', '2023-08-03');
-INSERT INTO public.tokens VALUES (1, 7, 'b917fc66-3c15-4efc-8f3f-4a02d7e39e75', '2023-08-03');
-INSERT INTO public.tokens VALUES (1, 8, '5a2c0d04-9e74-44ba-976c-63d25da11636', '2023-08-03');
-INSERT INTO public.tokens VALUES (1, 9, 'f8b6a69e-2397-4c82-9223-0eff3007dd03', '2023-08-03');
-INSERT INTO public.tokens VALUES (1, 10, '1d1f3826-d08c-4991-9256-54ed3a04f99d', '2023-08-03');
-INSERT INTO public.tokens VALUES (1, 11, '2185949d-2d05-4959-a063-a12c242e93ba', '2023-08-03');
-INSERT INTO public.tokens VALUES (1, 12, 'ae804cbe-97cf-4eff-bde2-ad29d27a9022', '2023-08-03');
-INSERT INTO public.tokens VALUES (1, 13, '8e7472d6-f5cf-409b-a7cf-777cecdaaea7', '2023-08-03');
-INSERT INTO public.tokens VALUES (1, 14, 'c6e7a866-f359-4bca-834b-d98bcc3ac918', '2023-08-05');
-INSERT INTO public.tokens VALUES (1, 15, '2356286d-5994-454b-8f93-349bf9eecddc', '2023-08-05');
+INSERT INTO public.tokens VALUES (1, 2, '42fa7213-c2be-4d33-a0f9-2250202d0a0d', '2023-08-03 00:00:00-03');
+INSERT INTO public.tokens VALUES (1, 3, '1de75c83-1c9a-4ff7-af72-b1d02229a6a8', '2023-08-03 00:00:00-03');
+INSERT INTO public.tokens VALUES (1, 4, 'f2b8fee2-b977-46d2-9f60-19735ebd50ed', '2023-08-03 00:00:00-03');
+INSERT INTO public.tokens VALUES (1, 5, 'e2cc1cd2-4c9c-4fca-913e-a5fe76e08bdf', '2023-08-03 00:00:00-03');
+INSERT INTO public.tokens VALUES (1, 6, 'de3bcf9c-db98-4b59-9692-92c18c2c9f23', '2023-08-03 00:00:00-03');
+INSERT INTO public.tokens VALUES (1, 7, 'b917fc66-3c15-4efc-8f3f-4a02d7e39e75', '2023-08-03 00:00:00-03');
+INSERT INTO public.tokens VALUES (1, 8, '5a2c0d04-9e74-44ba-976c-63d25da11636', '2023-08-03 00:00:00-03');
+INSERT INTO public.tokens VALUES (1, 9, 'f8b6a69e-2397-4c82-9223-0eff3007dd03', '2023-08-03 00:00:00-03');
+INSERT INTO public.tokens VALUES (1, 10, '1d1f3826-d08c-4991-9256-54ed3a04f99d', '2023-08-03 00:00:00-03');
+INSERT INTO public.tokens VALUES (1, 11, '2185949d-2d05-4959-a063-a12c242e93ba', '2023-08-03 00:00:00-03');
+INSERT INTO public.tokens VALUES (1, 12, 'ae804cbe-97cf-4eff-bde2-ad29d27a9022', '2023-08-03 00:00:00-03');
+INSERT INTO public.tokens VALUES (1, 13, '8e7472d6-f5cf-409b-a7cf-777cecdaaea7', '2023-08-03 00:00:00-03');
+INSERT INTO public.tokens VALUES (1, 14, 'c6e7a866-f359-4bca-834b-d98bcc3ac918', '2023-08-05 00:00:00-03');
+INSERT INTO public.tokens VALUES (1, 15, '2356286d-5994-454b-8f93-349bf9eecddc', '2023-08-05 00:00:00-03');
+INSERT INTO public.tokens VALUES (3, 16, 'f5c04f4c-f382-46a3-91b2-12774ecaf118', '2023-08-05 00:00:00-03');
 
 
 --
 -- Data for Name: urls; Type: TABLE DATA; Schema: public; Owner: -
 --
 
-INSERT INTO public.urls VALUES (1, '2023-08-05 12:37:08.113', '15', 'hjsaldui', '7o_5xhY-jz');
-INSERT INTO public.urls VALUES (3, '2023-08-05 12:37:21.419', '15', 'hjsaldui2', 'hH0iFfg-9C');
-INSERT INTO public.urls VALUES (4, '2023-08-05 12:37:28.488', '15', 'hjsaldui2cx', 'TNc43E4Ipj');
-INSERT INTO public.urls VALUES (5, '2023-08-05 12:37:31.522', '15', 'hjsaldui2cxsdf', 'rb3p851WO3');
-INSERT INTO public.urls VALUES (7, '2023-08-05 12:51:51.583', '15', 'hjsaldui2cxsdg', 's_twScjpIi');
-INSERT INTO public.urls VALUES (8, '2023-08-05 13:02:26.805', '15', 'https://www.google.com/search?channel=fs&client=ubuntu&q=pegar+valores+do+headers+node', '4vJY4KEdds');
-INSERT INTO public.urls VALUES (9, '2023-08-05 13:02:43.473', '15', 'https://www.google.com/', 'xqVk651IK7');
-INSERT INTO public.urls VALUES (12, '2023-08-05 13:08:37.797', '15', 'https://www.google2.com/', 'NiujGvuCcC');
-INSERT INTO public.urls VALUES (15, '2023-08-05 13:10:20.954', '15', 'https://www.google3.com/', 'YjxDN1kGKa');
+INSERT INTO public.urls VALUES (1, '2023-08-05 12:37:08.113', '15', 'hjsaldui', '7o_5xhY-jz', 0);
+INSERT INTO public.urls VALUES (3, '2023-08-05 12:37:21.419', '15', 'hjsaldui2', 'hH0iFfg-9C', 0);
+INSERT INTO public.urls VALUES (4, '2023-08-05 12:37:28.488', '15', 'hjsaldui2cx', 'TNc43E4Ipj', 0);
+INSERT INTO public.urls VALUES (5, '2023-08-05 12:37:31.522', '15', 'hjsaldui2cxsdf', 'rb3p851WO3', 0);
+INSERT INTO public.urls VALUES (7, '2023-08-05 12:51:51.583', '15', 'hjsaldui2cxsdg', 's_twScjpIi', 0);
+INSERT INTO public.urls VALUES (8, '2023-08-05 13:02:26.805', '15', 'https://www.google.com/search?channel=fs&client=ubuntu&q=pegar+valores+do+headers+node', '4vJY4KEdds', 0);
+INSERT INTO public.urls VALUES (12, '2023-08-05 13:08:37.797', '15', 'https://www.google2.com/', 'NiujGvuCcC', 0);
+INSERT INTO public.urls VALUES (15, '2023-08-05 13:10:20.954', '15', 'https://www.google3.com/', 'YjxDN1kGKa', 0);
+INSERT INTO public.urls VALUES (18, '2023-08-05 18:11:12.028', '16', 'https://www.google2432.com/', 'kdpn4ErkOc', 0);
+INSERT INTO public.urls VALUES (23, '2023-08-05 19:10:06.487', '16', 'https://www.google2432d.com/', 'U83ilyBiHd', 0);
+INSERT INTO public.urls VALUES (25, '2023-08-05 23:02:42.838', '16', 'https://www.google2432dg.com/', 'LPhvhvT41-', 0);
+INSERT INTO public.urls VALUES (27, '2023-08-05 23:05:01.999', '16', 'https://www.google243d2dg.com/', 'vkV2Zi5LFs', 0);
+INSERT INTO public.urls VALUES (9, '2023-08-05 13:02:43.473', '15', 'https://www.google.com/', 'xqVk651IK7', 2);
 
 
 --
 -- Name: Users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public."Users_id_seq"', 2, true);
+SELECT pg_catalog.setval('public."Users_id_seq"', 4, true);
 
 
 --
 -- Name: tokens_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public.tokens_id_seq', 15, true);
+SELECT pg_catalog.setval('public.tokens_id_seq', 16, true);
 
 
 --
 -- Name: urls_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public.urls_id_seq', 15, true);
+SELECT pg_catalog.setval('public.urls_id_seq', 27, true);
 
 
 --
--- src/routes/users_info/user.js
+++ src/routes/users_info/user.js
@@ -1,5 +1,8 @@
 import { Router } from "express";
+import tokenvalidade from "../../middlewares/token_validate.js";
+import my_profile from "../../controllers/my_profile_get.js";
 
-user = Router()
+const user = Router()
+user.get("/users/me",tokenvalidade,my_profile)
 
 export default user
