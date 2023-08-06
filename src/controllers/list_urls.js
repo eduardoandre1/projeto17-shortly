c8b0f01ed33e9f086ab622c1eeb12584c6a1f6ea
eduardo andre <eduardo_andre@id.uff.br>
2023-08-06 10:42:05 -0300

    fix lis erro status

--- src/controllers/list_urls.js
+++ src/controllers/list_urls.js
@@ -4,7 +4,7 @@ export default async function list_urls(req,res){
     try{
         const finder = await DB.query(`SELECT urls.id as id,urls.shorter_url as "shortUrl",urls.original_url as url FROM urls WHERE urls.id = $1`,[id])
         if(finder.rowCount === 0){
-            return res.sendStatus(401)
+            return res.sendStatus(404)
         }
         return res.status(200).send(finder.rows[0])
     }catch(err){return res.status(500).send(erro.message)}
