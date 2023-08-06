3b21df2f668c418d52fb9d43c640832d399ae190
eduardo andre <eduardo_andre@id.uff.br>
2023-08-06 17:28:20 -0300

    feat:get profile date

--- src/controllers/my_profile_get.js
+++ src/controllers/my_profile_get.js
@@ -1,3 +1,26 @@
+import DB from "../database/postgresSQL.js"
+
 export default async function my_profile(req,res){
-    
+    const {user_id} = req.finder.rows[0]
+    try{
+        const user_data = await DB.query(
+            `SELECT 
+            "Users".id as userid, "Users".name,
+            urls.id,urls.original_url as url,
+            urls.shorter_url as "shortUrl",
+            urls.viens as "visitCount"
+            FROM "Users"
+            JOIN urls
+            ON "Users".id = urls.user_id
+            WHERE "Users".id = $1`,[user_id])
+        const user_data_formated = {
+            id:user_data.rows[0].userid,
+            name:user_data.rows[0].name,
+            visitCount: user_data.rows.reduce((total,valor)=>{return total =total + valor.visitCount},0),
+            shortenedUrls: user_data.rows.map((value)=>{return{id:value.id,shortUrl:value.shortUrl,visitCount:value.visitCount}})
+        }
+        const g =user_data.rows.reduce((total,valor)=>total + valor.visitCount)
+        console.log(g)
+        return res.status(200).send(user_data_formated)
+    }catch(err){return res.status(500).send(err.message)}
 }
